ee398d1db21616ad61514b745e9ad8f54c7bbf11
Cole Peters <cole@colepeters.com>
2023-01-25 12:50:03 -0600

    Append extension to import

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import collection from './packages/collection/layout-collection'
+import collection from './packages/collection/layout-collection.js'
 import sidebar from './packages/sidebar/layout-sidebar.js'
 
 export {
